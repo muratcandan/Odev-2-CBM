6d4fcdb23527945070eee77452c684a553e5f43b
Murat Candan <muratcandan1997@gmail.com>
2022-08-12 13:34:05 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Board from './components/Board';
+import Board2 from './components/Board2';
+
+function App() {
+  return (
+    <div className='app'>
+         <Board/>
+    </div>
+    
+  );
+}
+
+export default App;
